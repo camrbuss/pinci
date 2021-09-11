3d162fe99e8fa87afb6a8d12757f9f44b7fd1c78
camrbuss <camrbuss@vt.edu>
2021-09-11 14:04:54 -0600

    left and right transform

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -2,26 +2,31 @@
 #![no_main]
 
 use cortex_m_rt::entry;
+use embedded_hal::digital::v2::InputPin;
 use hal::clocks::init_clocks_and_plls;
 use hal::gpio::DynPin;
 use hal::pac;
+use hal::pac::interrupt;
 use hal::sio::Sio;
 use hal::usb::UsbBus;
 use hal::watchdog::Watchdog;
-use keyberon::chording::{ChordDef, Chording};
 use keyberon::debounce::Debouncer;
 use keyberon::key_code;
+use keyberon::layout;
 use keyberon::layout::Layout;
 use keyberon::matrix::{Matrix, PressedKeys};
 use panic_halt as _;
 use rp2040_hal as hal;
-use usb_device::{class_prelude::*, prelude::*};
+use usb_device::class_prelude::*;
 
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER;
 
-static mut USB_DEVICE: Option<UsbDevice<UsbBus>> = None;
+static mut USB_DEVICE: Option<usb_device::device::UsbDevice<'_, rp2040_hal::usb::UsbBus>> = None;
+static mut USB_CLASS: Option<
+    keyberon::hid::HidClass<'_, rp2040_hal::usb::UsbBus, keyberon::keyboard::Keyboard<()>>,
+> = None;
 static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
 
 const SCAN_TIME_US: u32 = 1000;
@@ -34,21 +39,20 @@ pub enum CustomActions {
 // TODO: implement uf2 and reset
 // const DFU: Action<CustomActions> = Custom(CustomActions::Dfu);
 // const RESET: Action<CustomActions> = Custom(CustomActions::Reset);
-const HK_TAB: ChordDef = ChordDef::new((0, 0), &[(0, 0), (0, 1)]);
 
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers<CustomActions> = keyberon::layout::layout! {
     {[ // 0 TODO: make real layout
-        Q W E R T Y U I O P
-        A S D F G H J K L ;
-        Z X C V B N M , . /
-        1 2 3 4 
+        Q W E R T   Y U I O P
+        A S D F G   H J K L ;
+        Z X C V B   N M , . /
+        1 2 t t t   3 4 t t t
     ]}
     {[ // 1
-        1 2 3 4 5 6 7 8 9 0
-        A S D F G H J K L ;
-        Z X C V B N M , . /
-        1 2 3 4 
+        Q W E R T   Y U I O P
+        A S D F G   H J K L ;
+        Z X C V B   N M , . /
+        1 2 t t t   3 4 t t t
     ]}
 };
 #[entry]
@@ -79,8 +83,14 @@ fn main() -> ! {
         USB_BUS = Some(usb_bus);
     }
 
-    let mut usb_class = keyberon::new_class(unsafe { USB_BUS.as_ref().unwrap() }, ());
-    let mut usb_dev = keyberon::new_device(unsafe { USB_BUS.as_ref().unwrap() });
+    let usb_class = keyberon::new_class(unsafe { USB_BUS.as_ref().unwrap() }, ());
+    unsafe {
+        USB_CLASS = Some(usb_class);
+    }
+    let usb_dev = keyberon::new_device(unsafe { USB_BUS.as_ref().unwrap() });
+    unsafe {
+        USB_DEVICE = Some(usb_dev);
+    }
 
     let sio = Sio::new(pac.SIO);
     let pins = hal::gpio::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut resets);
@@ -104,6 +114,26 @@ fn main() -> ! {
     let gpio14 = pins.gpio14;
 
     let gpio20 = pins.gpio20;
+
+    // GPIO1 is high for the right hand side
+    let side = pins.gpio1.into_floating_input();
+    let is_right = side.is_high().unwrap();
+    let transform: fn(layout::Event) -> layout::Event = if is_right {
+        |e| {
+            e.transform(|i: u8, j: u8| -> (u8, u8) {
+                // 0 -> 5,  5 -> 15, 10 -> 25
+                let x = ((j / 5) * 10) + (j % 5) + 5;
+                (i, x)
+            })
+        }
+    } else {
+        |e| {
+            e.transform(|i: u8, j: u8| -> (u8, u8) {
+                let x = ((j / 5) * 10) + (j % 5);
+                (i, x)
+            })
+        }
+    };
 
     let led = pins.gpio25.into_push_pull_output();
 
@@ -135,23 +165,22 @@ fn main() -> ! {
 
     let mut layout = Layout::new(LAYERS);
     let mut debouncer = Debouncer::new(PressedKeys::default(), PressedKeys::default(), 30);
-    let mut chording = Chording::new(&[HK_TAB]);
     let mut last_time = pac.TIMER.timelr.read().bits();
+
+    // Enable USB interrupt
+    unsafe {
+        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    };
 
     loop {
         let current_time = pac.TIMER.timelr.read().bits();
         // TODO: fix overflow
         if (current_time - last_time) > SCAN_TIME_US {
             last_time = current_time;
-            if usb_dev.poll(&mut [&mut usb_class]) {
-                usb_class.poll();
-            }
-            let events = debouncer.events(matrix.get().unwrap());
-            for event in chording.tick(events.collect()).into_iter() {
+            for event in debouncer.events(matrix.get().unwrap()).map(transform) {
                 layout.event(event);
             }
             match layout.tick() {
-                // TODO: implement right and left transform
                 keyberon::layout::CustomEvent::Press(event) => match event {
                     CustomActions::Reset => {
                         cortex_m::peripheral::SCB::sys_reset();
@@ -161,9 +190,23 @@ fn main() -> ! {
                 _ => (),
             }
             let report: key_code::KbHidReport = layout.keycodes().collect();
-            if usb_class.device_mut().set_keyboard_report(report.clone()) {
-                while let Ok(0) = usb_class.write(report.as_bytes()) {}
+            // TODO: use rtic
+            // cortex_m::interrupt::free(|_| {
+            let usb_c = unsafe { USB_CLASS.as_mut().unwrap() };
+            if usb_c.device_mut().set_keyboard_report(report.clone()) {
+                while let Ok(0) = usb_c.write(report.as_bytes()) {}
             }
+            // });
         }
+    }
+}
+
+#[allow(non_snake_case)]
+#[interrupt]
+unsafe fn USBCTRL_IRQ() {
+    let usb_dev = USB_DEVICE.as_mut().unwrap();
+    let usb_class = USB_CLASS.as_mut().unwrap();
+    if usb_dev.poll(&mut [usb_class]) {
+        usb_class.poll();
     }
 }
