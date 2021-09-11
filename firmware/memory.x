9de93e789a39b6c66b7882d94f1ac25103011ac6
camrbuss <camrbuss@vt.edu>
2021-09-11 00:48:30 -0600

    usb enumeration a key presses

--- firmware/memory.x
+++ firmware/memory.x
@@ -1,18 +1,13 @@
-MEMORY
-{
-  /* NOTE 1 K = 1 KiBi = 1024 bytes */
-  /* RP2040 */
-  BOOT_LOADER : ORIGIN = 0x10000000, LENGTH = 0x100
-  FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
-  RAM : ORIGIN = 0x20000000, LENGTH = 264K
+MEMORY {
+    BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x100
+    FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
+    RAM   : ORIGIN = 0x20000000, LENGTH = 256K
 }
 
 SECTIONS {
-
-  /* Boot loader */
-  .boot_loader ORIGIN(BOOT_LOADER) :
-  {
-    KEEP(*(.boot_loader*));
-  } > BOOT_LOADER
-
+    /* ### Boot loader */
+    .boot2 ORIGIN(BOOT2) :
+    {
+        KEEP(*(.boot2));
+    } > BOOT2
 } INSERT BEFORE .text;
--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,22 +1,28 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
-use core::sync::atomic::{AtomicBool, Ordering};
 use cortex_m_rt::entry;
-use embedded_hal::digital::v2::OutputPin;
 use hal::clocks::init_clocks_and_plls;
 use hal::gpio::DynPin;
 use hal::pac;
 use hal::sio::Sio;
+use hal::usb::UsbBus;
 use hal::watchdog::Watchdog;
 use keyberon::chording::{ChordDef, Chording};
 use keyberon::debounce::Debouncer;
+use keyberon::key_code;
 use keyberon::layout::Layout;
 use keyberon::matrix::{Matrix, PressedKeys};
-use pac::interrupt;
 use panic_halt as _;
-use pio::SetDestination;
 use rp2040_hal as hal;
+use usb_device::{class_prelude::*, prelude::*};
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER;
+
+static mut USB_DEVICE: Option<UsbDevice<UsbBus>> = None;
+static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
 
 const SCAN_TIME_US: u32 = 1000;
 
@@ -45,17 +51,12 @@ pub static LAYERS: keyberon::layout::Layers<CustomActions> = keyberon::layout::l
         1 2 3 4 
     ]}
 };
-
-static ALARM: AtomicBool = AtomicBool::new(false);
-
 #[entry]
 fn main() -> ! {
     let pac = pac::Peripherals::take().unwrap();
-
     let mut resets = pac.RESETS;
-
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let _clocks = init_clocks_and_plls(
+    let clocks = init_clocks_and_plls(
         12_000_000u32,
         pac.XOSC,
         pac.CLOCKS,
@@ -67,7 +68,19 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    resets.reset.modify(|_r, w| w.usbctrl().clear_bit());
+    let usb_bus = UsbBusAllocator::new(UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut resets,
+    ));
+    unsafe {
+        USB_BUS = Some(usb_bus);
+    }
+
+    let mut usb_class = keyberon::new_class(unsafe { USB_BUS.as_ref().unwrap() }, ());
+    let mut usb_dev = keyberon::new_device(unsafe { USB_BUS.as_ref().unwrap() });
 
     let sio = Sio::new(pac.SIO);
     let pins = hal::gpio::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut resets);
@@ -92,12 +105,7 @@ fn main() -> ! {
 
     let gpio20 = pins.gpio20;
 
-    let mut led = pins.gpio25.into_push_pull_output();
-    let mut ledo = pins.gpio18.into_push_pull_output();
-
-    // TODO: make pin 8 and 9 a ps/2 interface
-    let _: hal::gpio::Pin<_, hal::gpio::FunctionPio0> = pins.gpio8.into_mode();
-    let _: hal::gpio::Pin<_, hal::gpio::FunctionPio0> = pins.gpio9.into_mode();
+    let led = pins.gpio25.into_push_pull_output();
 
     let mut matrix: Matrix<DynPin, DynPin, 17, 1> = cortex_m::interrupt::free(move |_cs| {
         Matrix::new(
@@ -128,45 +136,16 @@ fn main() -> ! {
     let mut layout = Layout::new(LAYERS);
     let mut debouncer = Debouncer::new(PressedKeys::default(), PressedKeys::default(), 30);
     let mut chording = Chording::new(&[HK_TAB]);
-
-    // sideset is mandatory for each instruction, 1 pin is needed, pindirs is set to data
-    let side_set = pio::SideSet::new(false, 2, false);
-    let mut a = pio::Assembler::new_with_side_set(side_set);
-    let mut bit_loop = a.label();
-    a.set_with_side_set(SetDestination::PINS, 1, 0);
-    a.pull_with_side_set(false, true, 1);
-    a.set_with_side_set(SetDestination::PINS, 0, 0);
-    a.set_with_side_set(SetDestination::X, 8, 1);
-    a.bind(&mut bit_loop);
-    a.out_with_side_set(pio::OutDestination::PINS, 1, 0);
-    a.jmp_with_side_set(pio::JmpCondition::XDecNonZero, &mut bit_loop, 1);
-
-    let program = a.assemble(None);
-
-    let pio = hal::pio::PIO::new(pac.PIO0, &mut resets);
-    let sm = &pio.state_machines()[0];
-
-    hal::pio::PIOBuilder::default()
-        .with_program(&program)
-        .buffers(hal::pio::Buffers::OnlyTx)
-        .out_pins(8, 1)
-        .set_pins(8, 1)
-        .side_set(side_set)
-        .side_set_pin_base(9)
-        .out_shift_direction(hal::pio::ShiftDirection::Right)
-        .in_shift_direction(hal::pio::ShiftDirection::Right)
-        .autopull(false)
-        .clock_divisor(5000.0) // TODO: document magic number
-        .build(&pio, sm)
-        .unwrap();
-
-    sm.set_enabled(true);
-
     let mut last_time = pac.TIMER.timelr.read().bits();
+
     loop {
         let current_time = pac.TIMER.timelr.read().bits();
+        // TODO: fix overflow
         if (current_time - last_time) > SCAN_TIME_US {
             last_time = current_time;
+            if usb_dev.poll(&mut [&mut usb_class]) {
+                usb_class.poll();
+            }
             let events = debouncer.events(matrix.get().unwrap());
             for event in chording.tick(events.collect()).into_iter() {
                 layout.event(event);
@@ -181,25 +160,10 @@ fn main() -> ! {
                 },
                 _ => (),
             }
-            for key in layout.keycodes() {
-                if key == keyberon::key_code::KeyCode::J {
-                    // TODO: parse keycodes correctly
-                    sm.push(0b110110001);
-                    led.set_high().unwrap();
-                    ledo.set_low().unwrap();
-                } else {
-                    // TODO: remove led debug
-                    led.set_low().unwrap();
-                    ledo.set_high().unwrap();
-                }
+            let report: key_code::KbHidReport = layout.keycodes().collect();
+            if usb_class.device_mut().set_keyboard_report(report.clone()) {
+                while let Ok(0) = usb_class.write(report.as_bytes()) {}
             }
         }
     }
-}
-
-#[allow(non_snake_case)]
-#[interrupt]
-fn TIMER_IRQ_0() {
-    ALARM.store(true, Ordering::Relaxed);
-    // TODO: implement timer to prevent overflow issue
 }
